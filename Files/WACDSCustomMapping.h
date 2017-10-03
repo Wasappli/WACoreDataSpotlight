1bea8f85c8b58a4807160f6ed75803a54ade5b02
Saul Moreno Abril <saul@appfluence.com>
2017-10-03 11:35:10 +0200

    Add initialiser to change the default value of "cleanValuesOnReplacement"

--- Files/WACDSCustomMapping.h
+++ Files/WACDSCustomMapping.h
@@ -33,6 +33,18 @@ typedef NSDate* _Nonnull (^WACDSMappingExpirationDateBuilder)(id _Nonnull object
                        searchableItemAttributeSetBuilder:(WACDSSearchableItemAttributeSetBuilder _Nonnull)searchableItemAttributeSetBuilder;
 
 /**
+ *  Init the mapping
+ *
+ *  @param objectEntityName                  The object entity name
+ *  @param uniqueIdentifierPattern           A pattern for unique identifier. Ex: booking_{#bookingID#}
+ *  @param searchableItemAttributeSetBuilder A block which will be called for each object to index. You need to return an attribute set
+ *  @param cleanValuesOnReplacement          A bool which is used to change the default behaviour. By default is NO.
+ *
+ *  @return a fresh mapping
+ */
+- (instancetype)initWithManagedObjectEntityName:(NSString *)objectEntityName uniqueIdentifierPattern:(NSString *)uniqueIdentifierPattern searchableItemAttributeSetBuilder:(WACDSSearchableItemAttributeSetBuilder)searchableItemAttributeSetBuilder cleanValuesOnReplacement:(BOOL)cleanValuesOnReplacement;
+
+/**
  *  Get the unique identifier from an object based on the pattern
  *
  *  @param object the object you want to index
